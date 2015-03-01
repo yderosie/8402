ed1fb4a6f9878b3ef7e938bd4937e5e23d0c558e
xiTope <xiTope@MacBook-Pro-de-Tom.local>
2015-03-01 01:43:10 +0100

    Adding A New Version of the board designer

--- draw_board.c
+++ draw_board.c
@@ -32,33 +32,3 @@ void	draw_borders(WINDOW *screen)
 		i++;
 	}
 }
-
-int		main(int argc, char *argv[])
-{
-	int parent_x;
-	int parent_y;
-	int new_x;
-	int new_y;
-
-	initscr();
-	getmaxyx(stdscr, parent_y, parent_x);
-	WINDOW *field = newwin(parent_y, parent_x, 0, 0);
-	draw_borders(field);
-	while(1)
-	{
-		getmaxyx(stdscr, new_y, new_x);
-		if (new_y != parent_y || new_x != parent_x)
-		{
-			parent_x = new_x;
-			parent_y = new_y;
-			wresize(field, new_y, new_x);
-			wclear(stdscr);
-			wclear(field);
-			draw_borders(field);
-		}
-		wrefresh(field);
-	}
-	delwin(field);
-	endwin();
-	return (0);
-}
